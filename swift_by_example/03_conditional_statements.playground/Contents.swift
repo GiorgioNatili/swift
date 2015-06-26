7bbb3d03aa770751b4556073133141ce9a0df03c
giorgionatili <g.natili@gnstudio.com>
2015-06-26 18:42:51 -0400

    Add examples about conditionals

---
+++ swift_by_example/03_conditional_statements.playground/Contents.swift
@@ -0,0 +1,70 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/*********************
+ * Number Comparison *
+ * < Less than
+ * <= Less than or equal
+ * > Greater than
+ * >= Greater than or equal
+ * == Equal
+ * != Not equal
+ *********************/
+
+// Number comparison
+if 7 % 2 == 2 {
+    
+    print("7 is even")     // What's wrong?
+
+} else {
+    
+    print("7 is odd")      // 7 is odd
+
+}
+
+// Number comparison with operator
+var money = 20
+var burgerPrice = 10
+
+if money >=  burgerPrice {
+    
+    print("pay burger")
+    money -= burgerPrice
+    
+} else {
+    
+    print("wash dishes")
+    
+}
+
+// Multiple conditions && and ||
+let users = 9
+if users >= 8 && users <= 16 {
+    
+    print("nice workshop")
+    
+}
+
+let dogs = 7, cats = 5
+if dogs == 7 ||  cats == 5 {
+    
+    print("you got a lot of pets!")
+    
+}
+
+/*********************
+ **** Excercise ******
+    
+ You are given a year, 
+ determine if it’s a leap
+ year.
+ Leap years occur every 4 
+ years.
+
+ ********************/
+
+// Your code here...
+
