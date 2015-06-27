40ab9ac101b448ee57e50c4264a76fec36fb83e8
giorgionatili <g.natili@gnstudio.com>
2015-06-27 05:47:18 -0400

    Optionals values

---
+++ swift-by-example/07_optionals_and_wrapping.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/********************
+***** Optionals *****
+********************/
+
+// Optionals and unwrapping
+var name:String?
+let randomNumber = arc4random_uniform(150)
+
+if randomNumber > 75 {
+    
+    name = "Giorgio"
+    
+}
+
+if let username = name {
+    
+    for var char in username.characters {
+        
+        print(char)
+    
+    }
+}
+
+// Force unwrapping
+let value:String? = "Test"
+
+print("We got a value \(value)")
+print("We got a value and I promise it's not nil \(value!)")
