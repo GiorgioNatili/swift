7832bc4fbc7a88f0131a439454b5b9df32ae8d68
giorgionatili <g.natili@gnstudio.com>
2015-09-05 23:02:52 -0400

    Add a tutorial about UIPickerView

---
+++ getting-started/GroceryUIPickerCoreData/GroceryUIPickerCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroceryUIPickerCoreData
+//
+//  Created by Giorgio Natili on 9/5/15.
+//  Copyright (c) 2015 io.webplatform.swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
