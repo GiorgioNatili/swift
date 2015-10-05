9175050d4372117da99c005808573d9e39224973
Dmitry Shamis <dshamis@hubspot.com>
2015-10-04 21:08:07 -0400

    Delete to do items using table view editing

--- tutorials/WritingPlist/WritingPlist/NotesTableViewController.swift
+++ tutorials/WritingPlist/WritingPlist/NotesTableViewController.swift
@@ -88,17 +88,15 @@ class NotesTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            removeNote(notes[indexPath.row])
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
