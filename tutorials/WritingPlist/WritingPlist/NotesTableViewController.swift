2d18222ea264f6195d2aec4b6dac970926cbc664
marcyreg <marcyreg@gmail.com>
2015-10-04 23:14:36 -0400

    add note complete, however need to fix reloadData call from NotesTable as the table is not reloading data when a note is added

--- tutorials/WritingPlist/WritingPlist/NotesTableViewController.swift
+++ tutorials/WritingPlist/WritingPlist/NotesTableViewController.swift
@@ -86,7 +86,7 @@ class NotesTableViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
+            plistManager.removeItem(notes.removeAtIndex(indexPath.row))
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
