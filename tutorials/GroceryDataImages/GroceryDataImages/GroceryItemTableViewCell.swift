8d4d215b7df1484225ae376472d04fd718d933cc
giorgionatili <g.natili@gnstudio.com>
2015-11-13 23:46:44 -0500

    Fix the custom class for the cell

---
+++ tutorials/GroceryDataImages/GroceryDataImages/GroceryItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroceryItemTableViewCell.swift
+//  GroceryDataImages
+//
+//  Created by Giorgio Natili on 11/13/15.
+//  Copyright © 2015 io.webplatform.swift. All rights reserved.
+//
+
+import UIKit
+
+class GroceryItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
