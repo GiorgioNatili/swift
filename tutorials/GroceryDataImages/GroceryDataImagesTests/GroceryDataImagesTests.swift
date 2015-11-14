7c4c0c65173d795f4f59b6f80a1f3760ef24db84
giorgionatili <g.natili@gnstudio.com>
2015-11-13 22:54:49 -0500

    Grocery list with image setup

---
+++ tutorials/GroceryDataImages/GroceryDataImagesTests/GroceryDataImagesTests.swift
@@ -0,0 +1,36 @@
+//
+//  GroceryDataImagesTests.swift
+//  GroceryDataImagesTests
+//
+//  Created by Giorgio Natili on 11/13/15.
+//  Copyright © 2015 io.webplatform.swift. All rights reserved.
+//
+
+import XCTest
+@testable import GroceryDataImages
+
+class GroceryDataImagesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
