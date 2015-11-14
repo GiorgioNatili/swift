5fda6f124aac0bdf72a7d2850bc4d4b257f994a3
giorgionatili <g.natili@gnstudio.com>
2015-11-14 03:17:29 -0500

    Add the segue identifier

--- tutorials/GroceryDataImages/GroceryDataImages/MainTableViewController.swift
+++ tutorials/GroceryDataImages/GroceryDataImages/MainTableViewController.swift
@@ -110,14 +110,22 @@ class MainTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "addItemSegue" {
+            
+            if let vc = segue.destinationViewController as? AddItemViewController{
+                // vc.dataManager = dataManager
+            }
+            
+        }
+        
     }
-    */
 
 }
